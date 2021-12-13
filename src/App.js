2886a1c2b63c78e7326a9afe3b965b2b0d169f74
cancanbolat <cancanbolat@ogr.bandirma.edu.tr>
2021-12-13 04:13:19 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Profile from './components/Profile/Profile';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='app'>
+        <Header></Header>
+        <Profile></Profile>
+      </div>
+    </Router>
   );
 }
 
